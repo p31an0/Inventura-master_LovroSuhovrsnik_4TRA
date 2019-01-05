a60d4f64bb1844618a2795a4dd000e332a5e4b3d
lovro.suhovršnik <lovro.suhovrnik@scv.si>
2019-01-05 09:52:47 +0100

    5. 1. 2019

--- Inventura/Dodaj.Designer.cs
+++ Inventura/Dodaj.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NazivIzdelkaTextBox = new System.Windows.Forms.TextBox();
+            this.CenaIzdelkaTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DodajIzdelekButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Naziv izdelka:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(418, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cena izdelka:";
+            // 
+            // NazivIzdelkaTextBox
+            // 
+            this.NazivIzdelkaTextBox.Location = new System.Drawing.Point(165, 22);
+            this.NazivIzdelkaTextBox.Name = "NazivIzdelkaTextBox";
+            this.NazivIzdelkaTextBox.Size = new System.Drawing.Size(247, 20);
+            this.NazivIzdelkaTextBox.TabIndex = 2;
+            // 
+            // CenaIzdelkaTextBox
+            // 
+            this.CenaIzdelkaTextBox.Location = new System.Drawing.Point(567, 22);
+            this.CenaIzdelkaTextBox.Name = "CenaIzdelkaTextBox";
+            this.CenaIzdelkaTextBox.Size = new System.Drawing.Size(116, 20);
+            this.CenaIzdelkaTextBox.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(360, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(239, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kategorija:";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DodajIzdelekButton
+            // 
+            this.DodajIzdelekButton.Location = new System.Drawing.Point(688, 98);
+            this.DodajIzdelekButton.Name = "DodajIzdelekButton";
+            this.DodajIzdelekButton.Size = new System.Drawing.Size(75, 23);
+            this.DodajIzdelekButton.TabIndex = 4;
+            this.DodajIzdelekButton.Text = "Dodaj";
+            this.DodajIzdelekButton.UseVisualStyleBackColor = true;
+            // 
+            // Dodaj
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 133);
+            this.Controls.Add(this.DodajIzdelekButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.CenaIzdelkaTextBox);
+            this.Controls.Add(this.NazivIzdelkaTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Dodaj";
             this.Text = "Dodaj";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NazivIzdelkaTextBox;
+        private System.Windows.Forms.TextBox CenaIzdelkaTextBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button DodajIzdelekButton;
     }
 }
--- Inventura/Dodaj.cs
+++ Inventura/Dodaj.cs
@@ -16,5 +16,10 @@ namespace Inventura
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
